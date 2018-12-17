8e70a74dafb7429002bd8f5137e07833592c1caa
Hadas <bekihs2003@gmail.com>
2018-12-17 12:37:35 +0200

    fix

---
+++ build/precache-manifest.c7ff852cfe375528cbb3ca898c349e9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b0f998681899cd19a85",
+    "url": "/static/js/main.9b0f9986.chunk.js"
+  },
+  {
+    "revision": "8671f741547f7e16de9f",
+    "url": "/static/js/1.8671f741.chunk.js"
+  },
+  {
+    "revision": "9e218ce25478f9fd015a5a12bd077c09",
+    "url": "/index.html"
+  }
+];
